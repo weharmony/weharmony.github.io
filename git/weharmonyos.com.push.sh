3c9ef1807d5ff38476e2b5df9074d72bd2553f7a
kuangyufei <kuangyufei@126.com>
2023-12-29 22:33:57 +0800

    https://weharmony.github.io     注解仓库：gitee.com/weharmony/kernel_liteos_a_note     开源鸿蒙：http://open.weharmonyos.com     鸿蒙论坛：http://bbs.weharmonyos.com     鸿蒙手册：http://doxygen.weharmonyos.com     公众号: 鸿蒙研究站

--- git/weharmonyos.com.push.sh
+++ git/weharmonyos.com.push.sh
@@ -14,9 +14,9 @@
 #cp -r ./.vuepress/dist/* ../output_web/weharmonyos.com/
 
 cd ../
-#sed -i 's/ab61e8f1e4e78f565ec18f58aa46b735/39485dba914ae63b24732b6a90688f0b/g' ../index.html
-#sed -i 's/6850197bffce7d5806ea/f524e15efb84b8c837e6/g' ../index.html
-#sed -i 's/4b5856543ca62d63c5dcec984148c5239f0431d9/033f64c3b6569ee020d95575e72fdca8aec5cc2d/g' ../index.html
+sed -i 's/ab61e8f1e4e78f565ec18f58aa46b735/39485dba914ae63b24732b6a90688f0b/g' ./index.html
+sed -i 's/6850197bffce7d5806ea/f524e15efb84b8c837e6/g' ./index.html
+sed -i 's/4b5856543ca62d63c5dcec984148c5239f0431d9/033f64c3b6569ee020d95575e72fdca8aec5cc2d/g' ./index.html
 #git init
 git add -A
 git commit -m '站点:http://weharmonyos.com
