c6f60ea2d08089b79ae523395fecc32a91969f38
kuangyufei <kuangyufei@126.com>
2023-12-29 22:35:17 +0800

    https://weharmony.github.io     注解仓库：gitee.com/weharmony/kernel_liteos_a_note     开源鸿蒙：http://open.weharmonyos.com     鸿蒙论坛：http://bbs.weharmonyos.com     鸿蒙手册：http://doxygen.weharmonyos.com     公众号: 鸿蒙研究站

--- git/github.io.weharmony.push.sh
+++ git/github.io.weharmony.push.sh
@@ -17,8 +17,8 @@
 #cd ../output_web/weharmony.github.io
 cd ../
 sed -i 's/39485dba914ae63b24732b6a90688f0b/ab61e8f1e4e78f565ec18f58aa46b735/g' ./index.html
-sed -i 's/ID：f524e15efb84b8c837e6/6850197bffce7d5806ea/g' ./index.html
-sed -i 's/secrets：033f64c3b6569ee020d95575e72fdca8aec5cc2d/4b5856543ca62d63c5dcec984148c5239f0431d9/g' ./index.html
+sed -i 's/f524e15efb84b8c837e6/6850197bffce7d5806ea/g' ./index.html
+sed -i 's/033f64c3b6569ee020d95575e72fdca8aec5cc2d/4b5856543ca62d63c5dcec984148c5239f0431d9/g' ./index.html
 
 #git init
 git add -A
